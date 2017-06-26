076f6afc85664113f40ee171751a5c33d9eb7580
InvisiblE182 <osobnimail@protonmail.com>
2017-06-26 11:41:35 +0200

    dd

--- player/MainWindow.xaml.cs
+++ player/MainWindow.xaml.cs
@@ -14,8 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-
+using Microsoft.Win32;
+using System.Windows.Threading;
 
 
 namespace player
@@ -26,8 +26,10 @@ namespace player
         private List<double> volume { get; set; }
         private List<string> songList { get; set; }
         private int songIdx { get; set; }
-        private  int previousSong { get; set; }
-
+        private int previousSong { get; set; }
+        public string item { get; set; }
+        public DispatcherTimer timer { get; set; }
+        public Object lastVolContent { get; set; }
 
         public MainWindow()
         {
@@ -40,14 +42,13 @@ namespace player
             songIdx = 0;
             songList = new List<string>();
             volume = new List<double>();
-
+            timer = new DispatcherTimer();
         }
         
-
         private void Shuffle_Click(object sender, RoutedEventArgs e)
         {
             if (shuffleButton.Background == Brushes.White)
-            {
+            {//se shuffle nefunguje zpět - hraje to tu samou co teď
                 shuffleButton.Background = Brushes.Aqua;
             } else
             {
@@ -78,8 +79,10 @@ namespace player
             if (playButton.Content != FindResource("Pause") && songList.Count != 0)
             {
                 playButton.Content = FindResource("Pause");
+                Timer_Start();
                 player.Play();
                 tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
+                prgBar.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
             }
             else
             {
@@ -92,33 +95,40 @@ namespace player
         {
             if (songList.Count != 0)
             {
+                prgBar.Value = 0;
                 if (shuffleButton.Background == Brushes.White)
                 {
+                    previousSong = songIdx;
                     songIdx += 1;
                     if (songIdx == songList.Count)//current song was last in playlist
                     {
                         songIdx = 0;
                     }
+
                     player.Open(new Uri(songList[songIdx]));
                     tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
                     if (playButton.Content == FindResource("Pause"))
                     {
+                        Timer_Start();
                         player.Play();
                     }
                 }
                 else
                 {
                     previousSong = songIdx;
-                    while (songIdx == previousSong)//thist prevents playing the same song again
+                    while (songIdx == previousSong)//this prevents playing the same song again
                     {
                         Random rnd = new Random();
                         songIdx = rnd.Next(0, songList.Count);
                     }
-                    player.Open(new Uri(songList[songIdx]));
-                    tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
+                    
                     if (playButton.Content == FindResource("Pause"))
                     {
+                        player.Open(new Uri(songList[songIdx]));
+                        tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
                         player.Play();
+                        Timer_Start();
+                        
                     }
                 }
             }
@@ -128,60 +138,90 @@ namespace player
         {
             if (songList.Count != 0)
             {
+                prgBar.Value = 0;
                 if (shuffleButton.Background == Brushes.White)
                 {
+                    previousSong = songIdx;
                     songIdx -= 1;
                     if (songIdx < 0)
                     {
                         songIdx = songList.Count - 1;
                     }
-                    player.Open(new Uri(songList[songIdx]));
-                    tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
+                    
                     if (playButton.Content == FindResource("Pause"))
                     {
+                        player.Open(new Uri(songList[songIdx]));
+                        tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
                         player.Play();
+                        Timer_Start();
                     }
-                } else
+                    else
+                    {
+                        player.Open(new Uri(songList[songIdx]));
+                        tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
+                    }
+                }
+                else
                 {
                     previousSong = songIdx;
-                    player.Open(new Uri(songList[songIdx]));
-                    tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
                     if (playButton.Content == FindResource("Pause"))
                     {
+                        player.Open(new Uri(songList[songIdx]));
+                        tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
                         player.Play();
+                        Timer_Start();
+                    }
+                    else
+                    {
+                        player.Open(new Uri(songList[songIdx]));
+                        tbPlaying.Text = "Now playing: " + playlist.Items[songIdx];
+                    }
+                    songIdx -= 1;
+                    if (songIdx < 0)
+                    {
+                        songIdx = songList.Count - 1;
                     }
                 }
             }
         }
 
-        
-
         private void Volume_Click(object sender, RoutedEventArgs e)
         {
-            
             if (player.IsMuted == false)
             {
                 player.IsMuted = true;
+                lastVolContent = volumeButton.Content;
                 volumeButton.Content = FindResource("Muted");
                 volumeButton.Background = Brushes.Aqua;
             }
             else
             {
                 player.IsMuted = false;
-                volumeButton.Content = FindResource("High Volume");
                 volumeButton.Background = Brushes.White;
+                volumeButton.Content = lastVolContent;
             }
         }
         
-
         private void VolumeSlider_ValueChanged(object sender, EventArgs e)
         {
-            player.Volume = volumeSlider.Value;
-        }
+            player.Volume = volumeSlider.Value/100; //player.Volume is in range 0.0 - 1.0
+            if (volumeSlider.Value > 66)
+            {
+                volumeButton.Content = FindResource("High Volume");
 
-        private void SongSlider_ValueChanged(object sender, EventArgs e)
-        {
-            
+            }
+            else if (volumeSlider.Value > 32)
+            {
+                volumeButton.Content = FindResource("Medium Volume");
+            }
+            else if (volumeSlider.Value > 0)
+            {
+                volumeButton.Content = FindResource("Low Volume");
+            }
+            else
+            {
+                volumeButton.Content = FindResource("Muted");
+            }
         }
 
         private void Open_Click(object sender, EventArgs e)
@@ -197,6 +237,8 @@ namespace player
             open.Filter = "All Files|*.mp3*";
             if (open.ShowDialog() == true)
             {
+                songList.Clear();
+                playlist.Items.Clear();
                 foreach (string file in open.FileNames)
                 {
                     songList.Add(file);
@@ -206,11 +248,27 @@ namespace player
                 {
                     playlist.Items.Add(System.IO.Path.GetFileName(s).Replace(".mp3", String.Empty));
                 }
-                tbPlaying.Text = "First up: " + playlist.Items[songIdx];
+                tbPlaying.Text = "First up: " + playlist.Items[songIdx];  
                 playlist.BorderBrush = Brushes.Gray;
-
             }
+        }
 
+        private void Timer_Start()
+        {
+
+            if (playButton.Content == FindResource("Pause"))
+            {
+                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            prgTime.Text = String.Format("{0} / {1}", player.Position.ToString(@"mm\:ss"), player.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+            prgBar.Value = player.Position.TotalSeconds;
         }
 
         private void Playlist_DoubleClick(object sender, RoutedEventArgs e)
@@ -218,9 +276,9 @@ namespace player
             songIdx = playlist.SelectedIndex;
             player.Open(new Uri(songList[songIdx]));
             tbPlaying.Text = "Now playing: " + playlist.Items[songIdx]; ;
+            Timer_Start();
             player.Play();
+            
         }
-
-
     }
 }
